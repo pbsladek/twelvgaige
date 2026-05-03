348ab1af26f887c82070b067e1f00a8c38a1aa52
Paul Sladek <opsetc@icloud.com>
2026-05-03 14:25:24 -0700

    Release v0.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Twelvgaige.MixProject do
   def project do
     [
       app: :twelvgaige,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
